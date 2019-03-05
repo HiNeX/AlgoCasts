5b88431362c8edc492fbffec4163f9c94bef47bc
Roman Hinex <roman@hinex.io>
2019-03-06 00:04:18 +0600

    Add anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,16 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const clear = (str) => {
+    return str.toLowerCase()
+        .replace(/[^A-Za-z0-9]+/g, '')
+        .split('')
+        .sort()
+        .join('')
+}
+
+function anagrams(stringA, stringB) {
+    return clear(stringA) === clear(stringB)
+}
 
 module.exports = anagrams;
