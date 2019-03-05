a9d817f0152e678815f50b6ff23cdff03c76b5fc
Roman Hinex <roman@hinex.io>
2019-03-06 00:49:42 +0600

    Add bst

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,34 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data
+        this.left = null
+        this.right = null
+    }
+    insert(data) {
+        if (this.left && this.data > data) {
+            this.left.insert(data)
+        } else if (this.right && this.data < data) {
+            this.right.insert(data)
+        } else if (this.data > data) {
+            this.left = new Node(data)
+        } else if (this.data < data) {
+            this.right = new Node(data)
+        }
+    }
+    contains(data) {
+        if (this.data === data) {
+            return this
+        }
+
+        if (!this.left && !this.right) {
+            return null
+        }
+
+        return data > this.data ? this.right.contains(data) : this.left.contains(data)
+    }
+}
 
 module.exports = Node;
