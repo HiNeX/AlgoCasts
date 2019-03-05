5b9eecb8c65cba296923231a870d4e81d111cd98
Roman Hinex <roman@hinex.io>
2019-03-06 01:33:05 +0600

    Add events

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,29 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.c = []
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    this.c.push({ eventName, callback })
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    const eventsList = this.c.filter(e => e.eventName === eventName)
+
+    for (const e of eventsList) {
+        e.callback()
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    this.c = this.c.filter(e => e.eventName !== eventName)
+  }
 }
 
 module.exports = Events;
